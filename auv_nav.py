1624cf8877bc09857f01c345c384ffbd16390965
Blair Thornton <Blair.insitu@gmail.com>
2017-08-31 00:25:50 +0100

    no message

--- auv_nav.py
+++ auv_nav.py
@@ -18,10 +18,22 @@ def generate_paths(filepath,ftype):
 
     Returns:
         interleaved navigation and imaging data with output options:
-            'acfr' - combined.auv.raw
-            'oplab' - nav_standard.json
+            'acfr' - combined.RAW.auv
+                PHINS_COMPASS: 1444452882.644 r: -2.29 p: 17.21 h: 1.75 std_r: 0 std_p: 0 std_h: 0
+                RDI: 1444452882.644 alt:200 r1:0 r2:0 r3:0 r4:0 h:1.75 p:17.21 r:-2.29 vx:0.403 vy:0 vz:0 nx:0 ny:0 nz:0 COG:0 SOG:0 bt_status:32768 h_true:0 p_gimbal:0 sv: 1500
+                PAROSCI: 1444452882.644 298.289
+                VIS: 1444452882.655 [1444452882.655] sx_073311_image0003805_AC.tif exp: 0
+                VIS: 1444452882.655 [1444452882.655] sx_073311_image0003805_FC.tif exp: 0
+                SSBL_FIX: 1444452883 ship_x: 402.988947 ship_y: 140.275056 target_x: 275.337171 target_y: 304.388346 target_z: 299.2 target_hr: 0 target_sr: 364.347071 target_bearing: 127.876747
 
-            'nav_standard.json'
+            'oplab' - nav_standard.json
+                [{"epoch_timestamp": 1501974125.926, "epoch_timestamp_dvl": 1501974125.875, "class": "measurement", "sensor": "phins", "frame": "body", "category": "velocity", "data": [{"xx_velocity": -0.075, "xx_velocity_std": 0.200075}, {"yy_velocity": 0.024, "yy_velocity_std": 0.200024}, {"zz_velocity": -0.316, "zz_velocity_std": 0.20031600000000002}]},
+                {"epoch_timestamp": 1501974002.1, "class": "measurement", "sensor": "phins", "frame": "inertial", "category": "orientation", "data": [{"heading": 243.777, "heading_std": 2.0}, {"roll": 4.595, "roll_std": 0.1}, {"pitch": 0.165, "pitch_std": 0.1}]},
+                {"epoch_timestamp": 1501974125.926, "epoch_timestamp_dvl": 1501974125.875, "class": "measurement", "sensor": "phins", "frame": "body", "category": "altitude", "data": [{"altitude": 31.53, "altitude_std": 0.3153}, {"sound_velocity": 1546.0, "sound_velocity_correction": 0.0}]},
+                {"epoch_timestamp": 1501974002.7, "epoch_timestamp_depth": 1501974002.674, "class": "measurement", "sensor": "phins", "frame": "inertial", "category": "depth", "data": [{"depth": -0.958, "depth_std": -9.58e-05}]}]
+            
+            
+
             
     """
     # initiate data and processing flags
@@ -181,7 +193,8 @@ def generate_paths(filepath,ftype):
         filename='nav_standard.json'        
         proc_flag=2
     elif ftype == 'acfr':# or (ftype is not 'acfr'):
-        filename='combined.auv.raw'
+        outpath = outpath +'/' + dRAWLOGS_cv
+        filename='combined.RAW.auv'
         proc_flag=2    
     
     # check if output specified is recognised and make file path if not exist
