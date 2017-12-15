6763ffe5afde8b7242303f964341777a698d2d8e
blair <b.thornton@soton.ac.uk>
2017-12-15 06:01:05 +0000

    dead reckoning placeholder

---
+++ lib_calculus/interpolate.py
@@ -0,0 +1,20 @@
+# interpolate
+
+# Scripts to interpolate values
+
+# Author: Blair Thornton
+# Date: 14/12/2017
+
+import math
+
+class interpolate:
+	def __init__(self, x_query, x_lower, x_upper, y_lower, y_upper):
+		return
+				
+
+	def __new__(cls, x_query, x_lower, x_upper, y_lower, y_upper):
+
+		y_query=(y_upper-y_lower)/(x_upper-x_lower)*(x_query-x_lower)+y_lower
+				
+
+		return y_query
---
+++ lib_localisation/dead_reckoning.py
@@ -0,0 +1,20 @@
+# dead_reckoning
+
+# Scripts to integrate velocities and return positions
+
+# Author: Blair Thornton
+# Date: 14/12/2017
+
+import math
+
+class dead_reckoning:
+	def __init__(self, time_now, time_previous, north_velocity_previous, east_velocity_previous, northings_previous, eastings_previous):
+		return
+				
+
+	def __new__(cls, time_now, time_previous, north_velocity_previous, east_velocity_previous, northings_previous, eastings_previous):
+
+		northings_now = (time_now - time_previous)*north_velocity_previous + northings_previous
+		eastings_now = (time_now - time_previous)*east_velocity_previous + eastings_previous				
+
+		return northings_now, eastings_now
