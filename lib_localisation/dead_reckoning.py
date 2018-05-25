c5adb285bd827b2b18c4a0a39361581901150492
Lim J.W <jwl1u17@soton.ac.uk>
2018-05-25 07:21:11 +0100

    modified dead reckoning formula to 1/2*(t1-t0)(v1+v0) for smoother path

--- lib_localisation/dead_reckoning.py
+++ lib_localisation/dead_reckoning.py
@@ -12,9 +12,14 @@ class dead_reckoning:
 		return
 				
 
-	def __new__(cls, time_now, time_previous, north_velocity_previous, east_velocity_previous, northings_previous, eastings_previous):
+	#def __new__(cls, time_now, time_previous, north_velocity_previous, east_velocity_previous, northings_previous, eastings_previous):
 
-		northings_now = (time_now - time_previous)*north_velocity_previous + northings_previous
-		eastings_now = (time_now - time_previous)*east_velocity_previous + eastings_previous				
+		#northings_now = (time_now - time_previous)*north_velocity_previous + northings_previous
+		#eastings_now = (time_now - time_previous)*east_velocity_previous + eastings_previous				
+	
+	def __new__(cls, time_now, time_previous, north_velocity_now, north_velocity_previous, east_velocity_now, east_velocity_previous, northings_previous, eastings_previous):
+
+		northings_now = ((time_now - time_previous)*(north_velocity_previous + north_velocity_now))/2 + northings_previous
+		eastings_now = ((time_now - time_previous)*(east_velocity_previous + east_velocity_now))/2 + eastings_previous
 
 		return northings_now, eastings_now
