721259932f79e4bc3bbb6d0e4619241bba30c1c2
Lim J.W <jwl1u17@soton.ac.uk>
2018-07-18 07:12:57 +0100

    modified usbl std uncertainty equation

--- lib_sensors/parse_usbl_dump.py
+++ lib_sensors/parse_usbl_dump.py
@@ -18,7 +18,7 @@ from lib_coordinates.latlon_wgs84 import metres_to_latlon
 data_list=[]
 
 class parse_usbl_dump:
-	def __init__(self, filepath, filename, label, category, timezone, timeoffset, latitude_reference, longitude_reference, ftype, outpath, fileoutname, fileout):
+	def __init__(self, filepath, filename, label, category, timezone, timeoffset, distance_std_offset, distance_std_factor, latitude_reference, longitude_reference, ftype, outpath, fileoutname, fileout):
 
 		# parser meta data    
 		class_string = 'measurement'
@@ -26,7 +26,8 @@ class parse_usbl_dump:
 		frame_string = 'inertial'
 		
 		# gaps std models
-		distance_std_factor = 1/100 # 1% uncertainty often quoted by ship's crew
+		# distance_std_factor = 1/100 # 1% uncertainty often quoted by ship's crew
+		# distance_std_offset = 5
 
 		# read in timezone
 		if isinstance(timezone, str):			
@@ -111,7 +112,7 @@ class parse_usbl_dump:
 						distance_full = line_split[11].split('=')														
 						distance= float(distance_full[1])
 						
-						distance_std = distance_std_factor*distance
+						distance_std = distance_std_factor*distance + distance_std_offset
 
 						lateral_distance_full = line_split[9].split('=')														
 						lateral_distance= float(lateral_distance_full[1])
