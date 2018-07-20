04ef5a6dd02ef192c313f7971d15c3fffcd5143e
Adrian <adrian@iis.u-tokyo.ac.jp>
2018-07-20 15:50:19 +0100

    Remove unnecessary whitespace (mainly trailing tabs) and fix indentation

--- lib_sensors/parse_interlacer.py
+++ lib_sensors/parse_interlacer.py
@@ -15,68 +15,67 @@ from operator import itemgetter
 #class parse_interlacer:
 #	def __init__(self, ftype, outpath, filename):
 def parse_interlacer(ftype, outpath, filename):
-		data = array('f')
-		value = []
-		data_original = []
-		data_ordered = []
+	data = array('f')
+	value = []
+	data_original = []
+	data_ordered = []
 
-		# debug
-		# bar_list=[]
-		# print("Length of foo_list:", len(foo_list)) # prints 0 on first run, 2 on second run
-		# print("Length of bar_list:", len(bar_list)) # prints 0 on first run, always
-		# foo_list.append("foo_list is part of the module.") 
-		# foo_list.append("What is added now will be there when this module is called next time.")
-		# bar_list.append("bar_list is a local variable.")
-		# bar_list.append("It will start empty when the module is called next time, regardless of what we do with it this time.")
-		# end debug
+	# debug
+	# bar_list=[]
+	# print("Length of foo_list:", len(foo_list)) # prints 0 on first run, 2 on second run
+	# print("Length of bar_list:", len(bar_list)) # prints 0 on first run, always
+	# foo_list.append("foo_list is part of the module.") 
+	# foo_list.append("What is added now will be there when this module is called next time.")
+	# bar_list.append("bar_list is a local variable.")
+	# bar_list.append("It will start empty when the module is called next time, regardless of what we do with it this time.")
+	# end debug
 
-		if ftype == 'oplab':
-				for filein in glob.glob(outpath + os.sep + filename):
-					try:
-						with open(filein, 'r') as json_file:						
-							data=json.load(json_file)
-							
-							for i in range(len(data)):
-								data_packet=data[i]																								
-								value.append(str(float(data_packet['epoch_timestamp'])))								
-					
-					except ValueError:					
-						print('Error: no data in JSON file')											
-					
-					# sort data in order of epoch_timestamp
-					sorted_index, sorted_items  = zip(*sorted([(i,e) for i,e in enumerate(value)], key=itemgetter(1)))					
+	if ftype == 'oplab':
+			for filein in glob.glob(outpath + os.sep + filename):
+				try:
+					with open(filein, 'r') as json_file:
+						data=json.load(json_file)
+						
+						for i in range(len(data)):
+							data_packet=data[i]
+							value.append(str(float(data_packet['epoch_timestamp'])))
 				
-				# store interlaced data in order of time
-				for i in range(len(data)):				
-					data_ordered.append((data[sorted_index[i]]))				
-
-				# write out interlaced json file
-				with open(outpath + os.sep + filename,'w') as fileout:
-					json.dump(data_ordered, fileout)
-
-		if ftype == 'acfr':
-			try:
-				with open(outpath + os.sep + filename, 'r') as acfr_file:	
-					for line in acfr_file.readlines():					
-						line_split = line.strip().split(':') 
-						line_split_tailed = line_split[1].strip().split(' ') 
-						value.append(str(line_split_tailed[0]))
-						data_original.append(line)
-
-			except ValueError:					
-					print('Error: no data in RAW.auv file')		
-
-			# sort data in order of epoch_timestamp
-			sorted_index, sorted_items  = zip(*sorted([(i,e) for i,e in enumerate(value)], key=itemgetter(1)))
+				except ValueError:
+					print('Error: no data in JSON file')
+				
+				# sort data in order of epoch_timestamp
+				sorted_index, sorted_items  = zip(*sorted([(i,e) for i,e in enumerate(value)], key=itemgetter(1)))
 			
-			for i in range(len(data_original)):				
-				data_ordered.append(data_original[sorted_index[i]])
+			# store interlaced data in order of time
+			for i in range(len(data)):
+				data_ordered.append((data[sorted_index[i]]))
 
-			# write out interlaced acfr file
-
+			# write out interlaced json file
 			with open(outpath + os.sep + filename,'w') as fileout:
-				for i in range(len(data_original)):				
-					fileout.write(str(data_ordered[i]))
+				json.dump(data_ordered, fileout)
 
-			fileout.close()
+	if ftype == 'acfr':
+		try:
+			with open(outpath + os.sep + filename, 'r') as acfr_file:
+				for line in acfr_file.readlines():
+					line_split = line.strip().split(':')
+					line_split_tailed = line_split[1].strip().split(' ')
+					value.append(str(line_split_tailed[0]))
+					data_original.append(line)
 
+		except ValueError:
+				print('Error: no data in RAW.auv file')
+
+		# sort data in order of epoch_timestamp
+		sorted_index, sorted_items  = zip(*sorted([(i,e) for i,e in enumerate(value)], key=itemgetter(1)))
+
+		for i in range(len(data_original)):
+			data_ordered.append(data_original[sorted_index[i]])
+
+		# write out interlaced acfr file
+
+		with open(outpath + os.sep + filename,'w') as fileout:
+			for i in range(len(data_original)):
+				fileout.write(str(data_ordered[i]))
+
+		fileout.close()
