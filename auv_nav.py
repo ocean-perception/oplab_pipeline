61b071838310bbad6be9a6f54bb6ba1e2417016e
Adrian <adrian@iis.u-tokyo.ac.jp>
2018-09-28 11:06:36 +0100

    Change the way the 3 targets (parse, visualise and process) are called and use argparse to parse the command line arguments. Enable calling the code also from a different python module or script and not only from the command line

--- auv_nav.py
+++ auv_nav.py
@@ -1,31 +1,78 @@
-# Import librarys
-import sys, os, time
-import math
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 16:44:40 2018
+
+@author: Adrian
+"""
+
+
+import os
+import argparse
 from lib_parse_data.parse_data import parse_data
-from lib_extract.extract_data import extract_data
 from lib_visualise.display_info import display_info
+from lib_extract.extract_data import extract_data
 
 
-def syntax_error():
-# incorrect usage message
-    print ("    auv_nav.py [arguments]")
-    print ("        required arguments (select one of these at a time):")
-    print ("            -i [path to root raw folder]")
-    print ("            -v [path to root processed folder where parsed data exists]")
-    print ("            -e [path to root processed folder where parsed data exists]")
-    print ("        optional arguments:")
-    print ("            -o [output format 'oplab' or 'acfr']")
-    print ("            -start [start date & time in YYYYMMDDhhmmss] (only for extract)")
-    print ("            -finish [finish date & time] YYYYMMDDhhmmss] (only for extract)")
-    print ("            -plot <output pdf plots option> (only for extract)")
-    print ("            -plotly <output interactive plots using plotly option> (only for extract)")
-    print ("            -csv <output csv files option> (only for extract)")
-    print ("            -DR <output dead reckoning files option> (only for extract)")
-    print ("            -PF <perform particle filter option> (only for extract)")
-    
-    return -1
+def main(args=None): # This notation makes it possible to call the module from the command line as well as from a different python module. When called from the command line args defaults to None, and parse_args() defaults to using sys.argv[1:]. When called from a python script or module, pass the arguments as list, e.g. main(["parse", "-h"]). This will populate the args parameter.
+    """
+    Main entry point for project.
+    Args:
+        args : list of arguments as if they were input in the command line. Leave it None to use sys.argv.
+    """
+	
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
 
+    # Subparsers for the 3 targets 'parse', 'visualise' and 'process'
+	 # Arguments starting with a double-dash (optionally completed with a single-dash, single letter abbreviation) are optional
+	 # Arugments without the double-dash prefix are positional arguments and therefore required
+    subparser_parse = subparsers.add_parser('parse', help="Parse raw data and converting it to an intermediate dataformat for further processing. Type auv_nav parse -h for help on this target.")
+    subparser_parse.add_argument('path', 
+        help="Folderpath where the (raw) input data is. Needs to be a subfolder of 'raw' and contain the mission.yaml configuration file.")
+    # prefixing the argument with -- means it's optional
+    subparser_parse.add_argument('-f', '--format', dest='format', 
+        default="oplab", 
+        help="Format in which the data is output. 'oplab' or 'acfr'. Default: 'oplab'.")
+    subparser_parse.set_defaults(func=call_parse_data)
 
+    subparser_visualise = subparsers.add_parser('visualise', help="Visualise data. Data needs to be saved in the intermediate data format generated using auv_nav.py parse. Type auv_nav visualise -h for help on this target.")
+    subparser_visualise.add_argument('path', 
+        help="Path of folder where the data to visualise is. The folder has to be generated using auv_nav parse.")
+    subparser_visualise.add_argument('-f', '--format', dest='format',
+        default="oplab", 
+        help="Format in which the data to be visualised is stored. 'oplab' or 'acfr'. Default: 'oplab'.")
+    subparser_visualise.set_defaults(func=call_visualise_data)
+
+    subparser_process = subparsers.add_parser('process', help="Process and/or convert data. Data needs to be saved in the intermediate data format generated using auv_nav.py parse. Type auv_nav process -h for help on this target.")
+    subparser_process.add_argument('path', 
+        help="Path to folder where the data to process is. The folder has to be generated using auv_nav parse.")
+    subparser_process.add_argument('-f', '--format', dest='format', 
+        default="oplab", 
+        help="Format in which the data to be processed is stored. 'oplab' or 'acfr'. Default: 'oplab'.")
+    subparser_process.add_argument('-s', '--start', dest='start_datetime', default='', 
+        help="Start date & time in YYYYMMDDhhmmss from which data will be processed. If not set, start at beginning of dataset.")
+    subparser_process.add_argument('-e', '--end', dest='end_datetime', default='', 
+        help="End date & time in YYYYMMDDhhmmss up to which data will be processed. If not set process to end of dataset.")
+    subparser_process.set_defaults(func=call_process_data)
+	
+    args = parser.parse_args(args)	
+    args.func(args)
+	
+
+def call_parse_data(args):
+    parse_data(args.path, args.format)
+
+	
+def call_visualise_data(args):
+    display_info(args.path + os.sep, args.format)
+	
+	
+def call_process_data(args):
+    if not is_subfolder_of(args.path, "processed"):
+        raise ValueError("The input directory you provided is not a subfolder of a folder called 'processed'")
+    extract_data(args.path + os.sep, args.format, args.start_datetime, args.end_datetime)
+
+	
 def is_subfolder_of(path, folder_name):
     for i in range(1,len(path)):
         if path[i]==folder_name:
@@ -34,65 +81,4 @@ def is_subfolder_of(path, folder_name):
 
 
 if __name__ == '__main__':
-
-    start_time = time.time()
-    # initialise flags
-    flag_i=False # input path set flag
-    flag_v=False # visualisation flag
-    flag_e=False # extraction of information flag
-    flag_o=False # output type set flag
-
-    start_datetime=''
-    finish_datetime=''
-    
-    # read in filepath and ftype
-    if (int((len(sys.argv)))) < 3:
-        print('Error: not enough arguments')
-        syntax_error()
-    else:   
-        # read in filepath, start time and finish time from function call
-        for i in range(math.ceil(len(sys.argv))):
-
-            option=sys.argv[i]
-        
-            if option == "-i":
-                filepath=sys.argv[i+1]
-                flag_i=True
-            elif option == "-v":
-                filepath=sys.argv[i+1]
-                flag_v=True
-            elif option == "-e":
-                filepath=sys.argv[i+1]
-                flag_e=True
-            elif option == "-o":
-                ftype=sys.argv[i+1]
-                flag_o=True
-            elif option == "-start":
-                start_datetime=sys.argv[i+1]
-            elif option == "-finish":
-                finish_datetime=sys.argv[i+1]
-            elif option[0] == '-':
-                print('Error: incorrect use')
-                sys.exit(syntax_error())
-
-        if flag_o ==False:
-            print('No ouput option specified. Using the default output option "oplab"')
-            ftype='oplab'
-
-        if flag_i ==True:
-            parse_data(filepath,ftype)
-
-        elif flag_v == True:
-            display_info(filepath + os.sep,ftype) # Previously there used to be a check, whether the file is in a subfolder of processed, but display_info doesn't require this to be the case
-
-        elif flag_e==True:
-            if not is_subfolder_of(filepath, "processed"):
-                raise ValueError("The input directory you provided is not a subfolder of a folder called 'processed'")
-            extract_data(filepath + os.sep,ftype,start_datetime,finish_datetime)
-
-        else:
-            print('Error: incorrect use')
-            syntax_error()
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("Time taken = {} mins".format(elapsed_time/60))
+    main()
