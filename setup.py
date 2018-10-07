2afa0a290380fa156d9581a7c255d43d5be37d7a
Miquel Massot <miquel.massot@gmail.com>
2018-10-07 21:56:56 +0100

    Update requirements and version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="auv_nav",
-    version="0.0.1",
+    version="0.0.1rc4",
     author="Jin Lim, Blair Thornton",
     author_email="author@example.com",
     description="Parsers for navigation data for oplab and acfr formats",
@@ -22,4 +22,12 @@ setuptools.setup(
             'auv_nav = auv_nav.auv_nav:main',
         ],
     },
+    install_requires=[
+        'matplotlib>=2.1.2',
+        'numpy>=1.14.5',
+        'pandas>=0.22.0',
+        'plotly>=2.5.1',
+        'prettytable>=0.7.2',
+        'PyYAML>=3.12'
+    ]
 )
