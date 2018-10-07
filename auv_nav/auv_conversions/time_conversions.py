3285b1ec15362f4140176903ef8a3c33d4965e27
Miquel Massot <miquel.massot@gmail.com>
2018-10-07 21:58:24 +0100

    time_conversions is now PEP8 compliant

--- auv_nav/auv_conversions/time_conversions.py
+++ auv_nav/auv_conversions/time_conversions.py
@@ -1,16 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2018, University of Southampton
+All rights reserved.
+"""
+
 from datetime import datetime, timedelta
 from pytz import reference
-import calendar, time
+import calendar
+import time
 
-def date_time_to_epoch(yyyy,mm,dd,HH,MM,SS,timezone_offset_to_utc):
-	utc_date_time = datetime(yyyy,mm,dd,HH,MM,SS) - timedelta(hours=timezone_offset_to_utc)
-	epochtime = calendar.timegm(utc_date_time.timetuple())
-	return epochtime
+
+def date_time_to_epoch(yyyy, mm, dd, hh, mm1, ss, timezone_offset_to_utc):
+    utc_date_time = (datetime(yyyy, mm, dd, hh, mm1, ss)
+                     - timedelta(hours=timezone_offset_to_utc))
+    epochtime = calendar.timegm(utc_date_time.timetuple())
+    return epochtime
+
 
 def epoch_to_localtime(epochtime):
-	localtime = time.localtime(epochtime)
-	return localtime
+    localtime = time.localtime(epochtime)
+    return localtime
+
 
 def get_localtimezone():
-	localtimezone = reference.LocalTimezone().tzname(datetime.now()) # string
-	return localtimezone
+    localtimezone = reference.LocalTimezone().tzname(datetime.now())  # string
+    return localtimezone
