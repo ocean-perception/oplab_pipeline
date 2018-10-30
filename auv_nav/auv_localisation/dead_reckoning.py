4e54d19a9c18b4f569a037c58e3363a85b161aee
Miquel Massot <miquel.massot@gmail.com>
2018-10-30 15:11:40 +0000

    Turn DR into function and PEP8

--- auv_nav/auv_localisation/dead_reckoning.py
+++ auv_nav/auv_localisation/dead_reckoning.py
@@ -1,25 +1,19 @@
-# dead_reckoning
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2018, University of Southampton
+All rights reserved.
+"""
+
 
 # Scripts to integrate velocities and return positions
-
-# Author: Blair Thornton
-# Date: 14/12/2017
-
-import math
-
-class dead_reckoning:
-	def __init__(self, time_now, time_previous, north_velocity_previous, east_velocity_previous, northings_previous, eastings_previous):
-		return
-				
-
-	#def __new__(cls, time_now, time_previous, north_velocity_previous, east_velocity_previous, northings_previous, eastings_previous):
-
-		#northings_now = (time_now - time_previous)*north_velocity_previous + northings_previous
-		#eastings_now = (time_now - time_previous)*east_velocity_previous + eastings_previous				
-	
-	def __new__(cls, time_now, time_previous, north_velocity_now, north_velocity_previous, east_velocity_now, east_velocity_previous, northings_previous, eastings_previous):
-
-		northings_now = ((time_now - time_previous)*(north_velocity_previous + north_velocity_now))/2 + northings_previous
-		eastings_now = ((time_now - time_previous)*(east_velocity_previous + east_velocity_now))/2 + eastings_previous
-
-		return northings_now, eastings_now
+def dead_reckoning(time_now, time_previous,
+                   north_velocity_now, north_velocity_previous,
+                   east_velocity_now, east_velocity_previous,
+                   northings_previous, eastings_previous):
+    northings_now = (((time_now - time_previous)
+                     * (north_velocity_previous + north_velocity_now))/2
+                     + northings_previous)
+    eastings_now = (((time_now - time_previous)
+                    * (east_velocity_previous + east_velocity_now))/2
+                    + eastings_previous)
+    return northings_now, eastings_now
