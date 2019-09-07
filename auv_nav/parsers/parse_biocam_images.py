03dfa83ca728412ffe4f5a39a8453102d2a83519
Miquel Massot <miquel.massot@gmail.com>
2019-09-07 15:02:56 +0000

    Fix issues with glob and folders for biocam

--- auv_nav/parsers/parse_biocam_images.py
+++ auv_nav/parsers/parse_biocam_images.py
@@ -6,6 +6,8 @@
 # Date: 31/08/2017
 
 import os
+import glob
+from pathlib import Path
 # from datetime import datetime
 
 # sys.path.append("..")
@@ -37,6 +39,7 @@ def parse_biocam_images(mission,
     sensor_string = 'biocam'
 
     timezone = mission.image.timezone
+    timezone_offset = 0
     timeoffset = mission.image.timeoffset
     filepath = mission.image.cameras[0].path
     camera1_label = mission.image.cameras[0].name
@@ -63,22 +66,29 @@ def parse_biocam_images(mission,
 
     # determine file paths
 
-    filepath = get_raw_folder(outpath / '..' / filepath)
-    all_list = os.listdir(str(filepath))
+    filepath1 = get_raw_folder(outpath / '..' / filepath / str(camera1_label + '_*/*.*'))
+    filepath2 = get_raw_folder(outpath / '..' / filepath / str(camera2_label + '_*/*.*'))
+
+    camera1_list = glob.glob(str(filepath1))
+    camera2_list = glob.glob(str(filepath2))
 
     camera1_filename = [
-        line for line in all_list if camera1_label in line and '.txt' not in line and '._' not in line]
+        line for line in camera1_list if '.txt' not in line and '._' not in line]
     camera2_filename = [
-        line for line in all_list if camera2_label in line and '.txt' not in line and '._' not in line]
+        line for line in camera2_list if '.txt' not in line and '._' not in line]
+
+    print(filepath1)
+    print(filepath2)
+    print(camera1_list[0])
+    print(camera1_list[1])
+    print('Found ' + str(len(camera2_filename) + len(camera1_filename)) + ' images!')
 
     data_list = []
     if ftype == 'acfr':
         data_list = ''
 
     def timestamp_from_filename(filename, timezone_offset, timeoffset):
-        filename_split = filename[i].strip().split('_')
-
-        # 20190902_080209_205989_20190902_080207_636631_pcom.tif
+        filename_split = filename.strip().split('_')
 
         date_string = filename_split[0]
         time_string = filename_split[1]
@@ -99,14 +109,16 @@ def parse_biocam_images(mission,
             hour = int(time_string[0:2])
             mins = int(time_string[2:4])
             secs = int(time_string[4:6])
-            msec = int(ms_time_string[0:3])
+            usec = int(ms_time_string[0:6])
 
+            if yyyy < 2000:
+                return 0
             epoch_time = date_time_to_epoch(
                 yyyy, mm, dd, hour, mins, secs, timezone_offset)
             # dt_obj = datetime(yyyy,mm,dd,hour,mins,secs)
             # time_tuple = dt_obj.timetuple()
             # epoch_time = time.mktime(time_tuple)
-            epoch_timestamp = float(epoch_time+msec/1000+timeoffset)
+            epoch_timestamp = float(epoch_time+usec/1e6+timeoffset)
             return epoch_timestamp
         epoch_timestamp = time_from_string(date_string,
                                            time_string,
@@ -121,13 +133,13 @@ def parse_biocam_images(mission,
         return epoch_timestamp, cam_epoch_timestamp
 
     for i in range(len(camera1_filename)):
-        t1, tc1 = timestamp_from_filename(camera1_filename[i],
+        t1, tc1 = timestamp_from_filename(Path(camera1_filename[i]).name,
                                           timezone_offset,
                                           timeoffset)
         stamp_pc1.append(str(t1))
         stamp_cam1.append(str(tc1))
     for i in range(len(camera2_filename)):
-        t2, tc2 = timestamp_from_filename(camera2_filename[i],
+        t2, tc2 = timestamp_from_filename(Path(camera2_filename[i]).name,
                                           timezone_offset,
                                           timeoffset)
         stamp_pc2.append(str(t2))
