9360b301d082e5e216d014a240e29d9f98bad3b2
Miquel Massot <miquel.massot@gmail.com>
2019-10-07 14:56:33 +0100

    Bump to version 0.0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="auv_nav",
-    version='0.0.2.1',
+    version='0.0.3.0',
     author="Miquel Massot, Takaki Yamada, Subhra Das, Jin Lim & Blair Thornton",
     author_email="miquel.massot-campos@soton.ac.uk",
     description="Toolchain for auv dive processing",
@@ -30,13 +30,22 @@ setuptools.setup(
         'auv_cal': ['default_yaml/*.yaml'],
         'correct_images': ['default_yaml/*.yaml']},
     install_requires=[
-        'matplotlib>=2.1.2',
-        'numpy>=1.14.5',
-        'pandas>=0.22.0',
-        'plotly>=2.5.1',
         'prettytable>=0.7.2',
         'PyYAML>=3.12',
-        'pynmea2>=1.7.0',
-        'scipy>=0.19.1'
+        'argparse>=1.1',
+        'colour_demosaicing>=0.1.4',
+        'opencv-python>=4.1.0',
+        'imageio>=2.5.0',
+        'joblib>=0.13.2',
+        'matplotlib>=3.1.0',
+        'numpy>=1.16.4',
+        'pandas>=0.23.4',
+        'pillow>=5.1.0',
+        'plotly>=3.6.1',
+        'prettytable>=0.7.2',
+        'pynmea2>=1.15.0',
+        'pytz>=2018.9',
+        'scipy>=1.2.1',
+        'tqdm>=4.32.2'
     ]
 )
