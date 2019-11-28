a231bd209c8c052c93faa393f1f950c618485aef
Miquel Massot <miquel.massot@gmail.com>
2019-11-28 14:23:43 +0000

    Add BodyVelocity test

--- auv_nav/tests/test_sensors.py
+++ auv_nav/tests/test_sensors.py
@@ -1,4 +1,6 @@
 from auv_nav.tools.interpolate import interpolate
+from auv_nav.sensors import BodyVelocity
+
 
 def test_interpolate():
     x_query = 150.0
@@ -10,3 +12,16 @@ def test_interpolate():
     assert y_query == 150.0
 
 
+def test_BodyVelocity():
+    b = BodyVelocity()
+    autosub_data = {
+        "eTime": [1574950320],
+        "Vnorth0": [10.0],  # mm/s
+        "Veast0": [10.0],
+        "Vdown0": [10.0],
+        "Verr0": [0.01],
+        "Verr0": [0.01],
+        "Verr0": [0.01]
+    }
+    b.from_autosub(autosub_data, 0)
+    assert b.x_velocity == -0.01 and b.valid()  # m/s
