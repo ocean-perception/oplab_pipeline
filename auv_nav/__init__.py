9d4326977f827826bfdee4d19a14443bfae23d36
Miquel Massot <miquel.massot@gmail.com>
2020-04-09 12:26:11 +0100

    Allow global imports for common classes
    With this commit, it is possible to import, for example, Console using  instead of having to know the entire folder structure

---
+++ auv_nav/__init__.py
@@ -0,0 +1,5 @@
+from auv_nav.console import *  # noqa
+from auv_nav.mission import *  # noqa
+from auv_nav.camera_system import *  # noqa
+from auv_nav.vehicle import *  # noqa
+from auv_nav.sensors import *  # noqa
