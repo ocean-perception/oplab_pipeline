8fa4f26a90fca8b89bc1384ffefc2a6726aab3c9
Adrian <adrian.bodenmann@soton.ac.uk>
2020-05-14 21:26:38 +0100

    Add tests for check_dirs_exist()

--- oplab/tests/test_folder_structure.py
+++ oplab/tests/test_folder_structure.py
@@ -1,8 +1,7 @@
 import unittest
 from pathlib import Path
 from oplab import get_processed_folders
-#from oplab import get_processed_folders
-from oplab.folder_structure import get_processed_folders # noqa
+from oplab import check_dirs_exist
 
 
 class TestFolderStructure(unittest.TestCase):
@@ -26,8 +25,23 @@ class TestFolderStructure(unittest.TestCase):
         path = Path(__file__).parents[0]
         path_raw = []
         path_raw.append(path / 'raw/test0')
-        path_raw.append(path / 'raw/test1')
+        path_raw.append(path / 'raw/test1/')
         path_processed = []
         path_processed.append(path / 'processed/test0')
-        path_processed.append(path / 'processed/test1')
+        path_processed.append(path / 'processed/test1/')
         self.assertEqual(path_processed, get_processed_folders(path_raw))
+
+    def test_check_dirs_exist_folders(self):
+        self.assertTrue(check_dirs_exist(Path(__file__).parents[0]))
+        paths = []
+        paths.append(Path(__file__).parents[0])
+        paths.append(Path(__file__).parents[1])
+        self.assertTrue(check_dirs_exist(paths))
+        paths.append(Path(__file__).parents[0] / 'non-exiting-folder/')
+        self.assertFalse(check_dirs_exist(paths))
+
+    def test_check_dirs_exist_files(self):
+        self.assertFalse(check_dirs_exist(Path(__file__)))
+        paths = []
+        paths.append(Path(__file__))
+        self.assertFalse(check_dirs_exist(paths))
