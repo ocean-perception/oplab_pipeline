0ade58a0217269ab4845b7f9c9f5b10f5e01e346
Subhra K. Das <subhra_k.das@soton.ac.uk>
2020-05-14 11:11:05 +0100

    modified test_distance_matrix_generation.py for removing redundant code

--- correct_images/tests/test_distance_matrix_generation.py
+++ correct_images/tests/test_distance_matrix_generation.py
@@ -30,11 +30,11 @@ class TestCaseCorrector(unittest.TestCase):
 		path_dummy_folder = Path(path_temp).resolve() / 'dummy_folder'
 		if not path_dummy_folder.exists():
 			path_dummy_folder.mkdir()
-		self.imagelist = []
+		imagelist = []
 		for i in range(10):
 			imagename = 'image_' + str(i) + '.tif'
 			imagepath = path_dummy_folder / imagename
-			self.imagelist.append(imagepath)
+			imagelist.append(imagepath)
 		imagenumbers = []
 		distances = []
 		min_altitude = 2
@@ -60,51 +60,21 @@ class TestCaseCorrector(unittest.TestCase):
 		distance_folder = path_dummy_folder / 'distance'
 		if not distance_folder.exists():
 			distance_folder.mkdir(parents=True)
-		self.path_dummy_folder = path_dummy_folder
+		self.path_processed = path_dummy_folder
 		self.distance_folder  = distance_folder 
 		self.distance_path  = distance_path
-		self.min_altitude = min_altitude
-		self.max_altitude = max_altitude
+		self.altitude_min = min_altitude
+		self.altitude_max = max_altitude
+		self.image_height = ACFR_IMG_HEIGHT
+		self.image_width = ACFR_IMG_WIDTH
+		self._imagelist = imagelist
+		
 		# set cameras list
 		self.cameras = ['RC', 'LC']
 		self.compare_shape = [ACFR_IMG_HEIGHT, ACFR_IMG_WIDTH]
 
-	def run_distance_matrix_generation(self):
-		# create Corrector object
-		corrector = Corrector(True)
-
-		# set Corrector:: attributes
-		corrector.distance_metric = 'altitude'
-		corrector.path_processed = self.path_dummy_folder
-		corrector.distance_path = self.distance_path
-		corrector._camera_image_file_list = 'none'
-		corrector.image_height = ACFR_IMG_HEIGHT
-		corrector.image_width = ACFR_IMG_WIDTH
-		corrector._imagelist = self.imagelist
-		corrector.distance_matrix_numpy_folder = self.distance_folder
-		corrector.altitude_min = self.min_altitude
-		corrector.altitude_max = self.max_altitude
-
-		for camera in self.cameras:
-			# set camera name
-			corrector.camera_name = camera
-			# test feature for chosen camera
-			corrector.generate_distance_matrix()
-			self.assertEqual(len(corrector._imagelist), 
-					len(corrector.distance_matrix_numpy_filelist),
-					'Length of distance matrix filelist does not match with imagelist')
-			for idx in corrector.altitude_based_filtered_indices:
-				distance_matrix = np.load(corrector.distance_matrix_numpy_filelist[idx])
-				self.assertEqual(distance_matrix.shape[0], self.compare_shape[0], 'Dimension mismatch: height')
-				self.assertEqual(distance_matrix.shape[1], self.compare_shape[1], 'Dimension mismatch: width')
-				out_of_range = (np.abs(distance_matrix) < corrector.altitude_min).any() or (np.abs(distance_matrix) > corrector.altitude_max).any()
-				self.assertEqual(out_of_range, False, 'distance matrix values out of altitude bounds error')
-
-	def test_acfr_distance_matrix_generation(self):
-		self.createACFRDummyDataset()
-		self.run_distance_matrix_generation()		
-
-	def test_sx3_distance_matrix_generation(self):
+	
+	def createSX3DummyDataset(self):
 		# create a dummy imagelist
 		path_temp = tempfile.mkdtemp()
 		message = 'Temporary folder created at ' + path_temp
@@ -114,7 +84,7 @@ class TestCaseCorrector(unittest.TestCase):
 			path_dummy_folder.mkdir()
 		imagelist = []
 		for i in range(10):
-			imagename = 'image_' + str(i) + '.raw'
+			imagename = 'image_' + str(i) + '.tif'
 			imagepath = path_dummy_folder / imagename
 			imagelist.append(imagepath)
 		imagenumbers = []
@@ -146,56 +116,27 @@ class TestCaseCorrector(unittest.TestCase):
 		if not distance_folder.exists():
 			distance_folder.mkdir(parents=True)
 
-		# create Corrector object
-		corrector = Corrector(True)
-
-		# set Corrector:: attributes
-		corrector.distance_metric = 'altitude'
-		corrector.path_processed = path_dummy_folder
-		corrector.distance_path = distance_path
-		corrector._camera_image_file_list = 'none'
-		corrector._imagelist = imagelist
-		corrector.distance_matrix_numpy_folder = distance_folder
-		corrector.altitude_min = min_altitude
-		corrector.altitude_max = max_altitude
 		
+		# set attributes
+		self.distance_metric = 'altitude'
+		self.path_processed = path_dummy_folder
+		self.distance_path = distance_path
+		self.distance_folder = distance_folder
+		self._camera_image_file_list = 'none'
+		self._imagelist = imagelist
+		self.image_height = SX3_IMG_HEIGHT
+		self.image_width = SX3_IMG_WIDTH
 
+		self.altitude_min = min_altitude
+		self.altitude_max = max_altitude
+		
 		# set cameras list
-		cameras = []
-		cameras.append('Cam51707923')
-		cameras.append('Cam51707925')
-		cameras.append('LM165')
+		self.cameras = ['Cam51707923', 'Cam51707925', 'LM165']
+		self.compare_shape = [SX3_IMG_HEIGHT, SX3_IMG_WIDTH]
 
-		for camera in cameras:
-			# set camera name
-			corrector.camera_name = camera
-			if camera == 'Cam51707923' or camera == 'Cam51707925':
-				corrector.image_height = SX3_IMG_HEIGHT
-				corrector.image_width = SX3_IMG_WIDTH
-			elif camera == 'LM165':
-				corrector.image_height = LM165_IMG_HEIGHT
-				corrector.image_width = LM165_IMG_WIDTH
 
-			# test feature for chosen camera
-			corrector.generate_distance_matrix()
-			
-			self.assertEqual(len(corrector._imagelist), 
-					len(corrector.distance_matrix_numpy_filelist),
-					'Length of distance matrix filelist does not match with imagelist')
-			for idx in corrector.altitude_based_filtered_indices:
-				distance_matrix = np.load(corrector.distance_matrix_numpy_filelist[idx])
-				if camera == 'Cam51707923' or camera == 'Cam51707925':
-					self.assertEqual(distance_matrix.shape[0], SX3_IMG_HEIGHT, 'Dimension mismatch: height')
-					self.assertEqual(distance_matrix.shape[1], SX3_IMG_WIDTH, 'Dimension mismatch: width')
-				elif camera == 'LM165':
-					self.assertEqual(distance_matrix.shape[0], LM165_IMG_HEIGHT, 'Dimension mismatch: height')
-					self.assertEqual(distance_matrix.shape[1], LM165_IMG_WIDTH, 'Dimension mismatch: width')
-				out_of_range = (np.abs(distance_matrix) < corrector.altitude_min).any() or (np.abs(distance_matrix) > corrector.altitude_max).any()
-				self.assertEqual(out_of_range, False, 'distance matrix values out of altitude bounds error')
-		Console.info('Test done for SX3 images...')
-		# path_dummy_folder.unlink()
 
-	def test_biocam_distance_matrix_generation(self):
+	def createBiocamDummyDataset(self):
 		# create a dummy imagelist
 		path_temp = tempfile.mkdtemp()
 		message = 'Temporary folder created at ' + path_temp
@@ -205,7 +146,7 @@ class TestCaseCorrector(unittest.TestCase):
 			path_dummy_folder.mkdir()
 		imagelist = []
 		for i in range(10):
-			imagename = 'image_biocam' + str(i) + '.tif'
+			imagename = 'image_' + str(i) + '.tif'
 			imagepath = path_dummy_folder / imagename
 			imagelist.append(imagepath)
 		imagenumbers = []
@@ -213,7 +154,7 @@ class TestCaseCorrector(unittest.TestCase):
 		min_altitude = 2
 		max_altitude = 6
 		for i in range(15):
-			imagenumbers.append('image_biocam'+str(i))
+			imagenumbers.append('image_'+str(i))
 			distance = np.random.uniform(low=min_altitude-1, high=max_altitude+2)
 			distances.append(distance)
 		dataframe = pd.DataFrame({'Imagenumber':imagenumbers, ' Altitude [m]':distances})
@@ -235,46 +176,79 @@ class TestCaseCorrector(unittest.TestCase):
 		if not distance_folder.exists():
 			distance_folder.mkdir(parents=True)
 
+
+		# create the folder where distance numpy files will be generated
+		distance_folder = path_dummy_folder / 'distance'
+		if not distance_folder.exists():
+			distance_folder.mkdir(parents=True)
+
+		
+		# set attributes
+		self.distance_metric = 'altitude'
+		self.path_processed = path_dummy_folder
+		self.distance_path = distance_path
+		self.distance_folder = distance_folder
+		self._camera_image_file_list = 'none'
+		self._imagelist = imagelist
+		self.image_height = BIOCAM_IMG_HEIGHT
+		self.image_width = BIOCAM_IMG_WIDTH
+
+		self.altitude_min = min_altitude
+		self.altitude_max = max_altitude
+		
+
+		# set cameras list
+		self.cameras = ['cam61003146', 'cam61004444']
+		self.compare_shape = [BIOCAM_IMG_HEIGHT, BIOCAM_IMG_WIDTH]
+
+
+
+
+	def run_distance_matrix_generation(self):
 		# create Corrector object
 		corrector = Corrector(True)
 
 		# set Corrector:: attributes
 		corrector.distance_metric = 'altitude'
-		corrector.path_processed = path_dummy_folder
-		corrector.distance_path = distance_path
+		corrector.path_processed = self.path_processed
+		corrector.distance_path = self.distance_path
 		corrector._camera_image_file_list = 'none'
-		corrector._imagelist = imagelist
-		corrector.distance_matrix_numpy_folder = distance_folder
-		corrector.altitude_min = min_altitude
-		corrector.altitude_max = max_altitude
-		corrector.image_height = BIOCAM_IMG_HEIGHT
-		corrector.image_width = BIOCAM_IMG_WIDTH
-		
+		corrector.image_height = self.image_height
+		corrector.image_width = self.image_width
+		corrector._imagelist = self._imagelist
+		corrector.distance_matrix_numpy_folder = self.distance_folder
+		corrector.altitude_min = self.altitude_min
+		corrector.altitude_max = self.altitude_max
 
-		# set cameras list
-		cameras = []
-		cameras.append('cam61003146')
-		cameras.append('cam61004444')
-
-		for camera in cameras:
+		for camera in self.cameras:
 			# set camera name
 			corrector.camera_name = camera
-
 			# test feature for chosen camera
 			corrector.generate_distance_matrix()
-			
 			self.assertEqual(len(corrector._imagelist), 
 					len(corrector.distance_matrix_numpy_filelist),
 					'Length of distance matrix filelist does not match with imagelist')
 			for idx in corrector.altitude_based_filtered_indices:
 				distance_matrix = np.load(corrector.distance_matrix_numpy_filelist[idx])
-				self.assertEqual(distance_matrix.shape[0], BIOCAM_IMG_HEIGHT, 'Dimension mismatch: height')
-				self.assertEqual(distance_matrix.shape[1], BIOCAM_IMG_WIDTH, 'Dimension mismatch: width')
+				self.assertEqual(distance_matrix.shape[0], self.compare_shape[0], 'Dimension mismatch: height')
+				self.assertEqual(distance_matrix.shape[1], self.compare_shape[1], 'Dimension mismatch: width')
 				out_of_range = (np.abs(distance_matrix) < corrector.altitude_min).any() or (np.abs(distance_matrix) > corrector.altitude_max).any()
 				self.assertEqual(out_of_range, False, 'distance matrix values out of altitude bounds error')
-		Console.info('Test done for biocam images...')
-		
 
+	def test_acfr_distance_matrix_generation(self):
+		self.createACFRDummyDataset()
+		self.run_distance_matrix_generation()		
+
+	
+	def test_sx3_distance_matrix_generation(self):
+		self.createSX3DummyDataset()
+		self.run_distance_matrix_generation()
+
+
+	def test_biocam_distance_matrix_generation(self):
+		self.createBiocamDummyDataset()
+		self.run_distance_matrix_generation()
+	
     		
 if __name__ == '__main__':
     unittest.main()
