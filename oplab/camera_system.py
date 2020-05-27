f9a3b199e16e3d690c6664c99934d4c3f0f21a05
Miquel Massot <miquel.massot@gmail.com>
2020-05-27 14:45:58 +0100

    Fix for ACFR standard images and globbing

--- oplab/camera_system.py
+++ oplab/camera_system.py
@@ -63,11 +63,24 @@ class CameraEntry:
             glob_vec = raw_dir.glob(pre_glob)    
             img_dirs = [k for k in glob_vec]
             img_dir = Path(str(img_dirs[0]) + '/' + str(split_glob[1]))
+            for i in img_dir.rglob('*.' + self.extension):
+                self._image_list.append(str(i))
+        elif len(split_glob) == 3:  # path/i*/*LC*
+            # split = ['path/i', '/', 'LC]
+            pre_glob = split_glob[0] + '*'
+            glob_vec = raw_dir.glob(pre_glob)    
+            img_dirs = [k for k in glob_vec]
+            img_dir = Path(str(img_dirs[0]))
+            Console.info('Looking for images with the pattern *', img_dirs[2], '*')
+            for i in img_dir.glob('*' + img_dirs[2] + '*.' + self.extension):
+                self._image_list.append(str(i))
         elif len(split_glob) == 1:
             img_dir = self.path
+            for i in img_dir.rglob('*.' + self.extension):
+                self._image_list.append(str(i))
         else:
-            Console.error('Multiple globbing is not supported.')
-        [self._image_list.append(str(_)) for _ in img_dir.rglob('*.' + self.extension)]
+            Console.quit('Multiple globbing is not supported.')
+        
         self._image_list.sort()
         return self._image_list
 
