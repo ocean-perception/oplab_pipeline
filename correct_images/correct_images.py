a21b38c31f436e731f6a69d94461bdac9eeb44a2
Subhra K. Das <subhra_k.das@soton.ac.uk>
2020-05-29 19:24:15 +0100

    added header to the code files

--- correct_images/correct_images.py
+++ correct_images/correct_images.py
@@ -1,3 +1,12 @@
+ # -*- coding: utf-8 -*-
+"""
+Copyright (c) 2020, University of Southampton
+All rights reserved.
+Licensed under the BSD 3-Clause License. 
+See LICENSE.md file in the project root for full license information.  
+"""
+
+
 # this is the main entry point to correct images
 # IMPORT --------------------------------
 # all imports go here 
