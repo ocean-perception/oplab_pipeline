Fix wrong dimensions

correct_images/tools/numerical.py
@@ -91,6 +91,7 @@ def running_mean_std(file_list, loader=default.loader):
 
 
 def median_array(data: np.ndarray) -> np.ndarray:
+    # print("median_array", data.shape)
     if len(data.shape) == 3:
         # Monochrome
         return median_array_impl(data)
@@ -104,7 +105,10 @@ def median_array(data: np.ndarray) -> np.ndarray:
 
 
 def median_array_impl(data: np.ndarray) -> np.ndarray:
-    [n, a, b] = data.shape
+    # print("median_array_impl", data.shape)
+    n = data.shape[0]
+    a = data.shape[1]
+    b = data.shape[2]
     median_array = np.zeros((a, b), dtype=np.float32)
     for i in range(a):
         for j in range(b):
@@ -124,26 +128,12 @@ def median_array_impl(data: np.ndarray) -> np.ndarray:
 
 
 @njit
-def mean_array(data: np.ndarray) -> np.ndarray:
-    [n, a, b] = data.shape
-    mean_array = np.zeros((a, b), dtype=np.float32)
-    for i in range(a):
-        for j in range(b):
-            mean = 0.0
-            for k in range(n):
-                count = k + 1
-                new_value = data[k, i, j]
-                delta = new_value - mean
-                mean += delta / count
-            mean_array[i, j] = mean
-    return mean_array
-
-
-@njit
 def mean_std_array(data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    n = data.shape[0]
-    a = data.shape[1]
-    b = data.shape[2]
+    # print("mean_std_array", data.shape)
+    n = 1
+    a, b = data.shape[1], data.shape[2]
+    if len(data.shape) == 4:
+        n = data.shape[3]
 
     mean_array = np.zeros((a, b), dtype=np.float32)
     std_array = np.zeros((a, b), dtype=np.float32)
@@ -207,10 +197,14 @@ def image_mean_std_trimmed(data, ratio_trimming=0.2, calculate_std=True):
     numpy.ndarray
         Trimmed mean and std
     """
-
-    [n, a, b] = data.shape
-    ret_mean = np.zeros((a, b), np.float32)
-    ret_std = np.zeros((a, b), np.float32)
+    n = data.shape[0]
+    a = data.shape[1]
+    b = data.shape[2]
+    c = 1
+    if len(data.shape) == 4:
+        c = data.shape[3]
+    ret_mean = np.zeros((a, b, c), np.float32)
+    ret_std = np.zeros((a, b, c), np.float32)
 
     effective_index = [list(range(0, n))]
 
@@ -219,26 +213,26 @@ def image_mean_std_trimmed(data, ratio_trimming=0.2, calculate_std=True):
         + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     )
 
-    if ratio_trimming <= 0:
-        ret_mean, ret_std = mean_std_array(data)
-        return ret_mean, ret_std
-
-    else:
-
-        for idx_a in trange(a, ascii=True, desc=message):
-            results = [None] * b
-            for idx_b in range(b):
-                results[idx_b] = calc_mean_and_std_trimmed(
-                    data[effective_index, idx_a, idx_b][0],
-                    ratio_trimming,
-                    calculate_std,
-                )
-            ret_mean[idx_a, :] = np.array(results)[:, 0]
-            ret_std[idx_a, :] = np.array(results)[:, 1]
-        if not calculate_std:
-            return ret_mean
-        else:
+    for ch in range(c):
+        if ratio_trimming <= 0:
+            ret_mean, ret_std = mean_std_array(data[:, :, :, ch])
             return ret_mean, ret_std
+
+        else:
+            for idx_a in trange(a, ascii=True, desc=message):
+                results = [None] * b
+                for idx_b in range(b):
+                    results[idx_b] = calc_mean_and_std_trimmed(
+                        data[effective_index, idx_a, idx_b, ch],
+                        ratio_trimming,
+                        calculate_std,
+                    )
+                ret_mean[idx_a, :, ch] = np.array(results)[:, 0]
+                ret_std[idx_a, :, ch] = np.array(results)[:, 1]
+    if not calculate_std:
+        return ret_mean
+    else:
+        return ret_mean, ret_std
 
 
 def calc_mean_and_std_trimmed(data, rate_trimming, calc_std=True):