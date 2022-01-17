fc972cc0a3909a649cfb93d5c76a4ef9237660ca
Miquel Massot <miquel.massot@gmail.com>
2022-01-17 13:54:29 +0000

    Add suport for ROSBAG in auv_nav and correct_images

--- src/correct_images/loaders/rosbag.py
+++ src/correct_images/loaders/rosbag.py
@@ -9,13 +9,14 @@ except ImportError:
 # fmt: on
 
 
-def loader(img_file, bagfile, src_bit=None):
+def loader(img_timestamp, img_topic, bagfile_list, src_bit=None):
     """Load an image from a ROS bagfile"""
     if not ROSBAG_IS_AVAILABLE:
         raise ImportError("ROS bagfile support is not available.")
     bridge = CvBridge()
-    with rosbag.Bag(str(bagfile)) as bag:
-        for topic, msg, t in bag.read_messages(topics=[str(img_file)]):
-            if topic == str(img_file):
-                return bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    for bagfile in bagfile_list:
+        with rosbag.Bag(str(bagfile)) as bag:
+            for topic, msg, t in bag.read_messages(topics=[str(img_topic)]):
+                if t == img_timestamp and topic == img_topic:
+                    return bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
     return None
