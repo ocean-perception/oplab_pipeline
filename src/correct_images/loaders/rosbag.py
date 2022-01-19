0f680c349b07cf49bab59f9a164e0a2abd6f6c0c
Miquel Massot <miquel.massot@gmail.com>
2022-01-19 15:24:10 +0000

    Fix rosbag parsing and filtering

--- src/correct_images/loaders/rosbag.py
+++ src/correct_images/loaders/rosbag.py
@@ -4,6 +4,7 @@ from auv_nav.sensors import ros_stamp_to_epoch
 ROSBAG_IS_AVAILABLE = False
 try:
     import rosbag
+    import rospy
     from cv_bridge import CvBridge
     ROSBAG_IS_AVAILABLE = True
 except ImportError:
@@ -11,6 +12,7 @@ except ImportError:
 # fmt: on
 
 TOL = 1e-3
+SEARCH_TOL = 0.5
 
 
 def loader(img_timestamp, img_topic, bagfile_list, tz_offset_s, src_bit=None):
@@ -20,13 +22,20 @@ def loader(img_timestamp, img_topic, bagfile_list, tz_offset_s, src_bit=None):
     bridge = CvBridge()
     img_timestamp = float(img_timestamp)
     # print("Loading image at", img_timestamp)
+
+    # Bagfile filtering times
+    start_time = rospy.Time.from_sec(img_timestamp - SEARCH_TOL + tz_offset_s)
+    end_time = rospy.Time.from_sec(img_timestamp + SEARCH_TOL + tz_offset_s)
+
     for bagfile in bagfile_list:
         with rosbag.Bag(str(bagfile)) as bag:
-            for topic, msg, t in bag.read_messages(topics=[str(img_topic)]):
+            for topic, msg, t in bag.read_messages(
+                topics=[str(img_topic)], start_time=start_time, end_time=end_time,
+            ):
                 epoch_timestamp = ros_stamp_to_epoch(msg.header.stamp) - tz_offset_s
                 # print(abs(epoch_timestamp - img_timestamp))
                 if abs(epoch_timestamp - img_timestamp) < TOL:
-                    # print("Image found!")
+                    # print("Image found at", t.to_sec())
                     return bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-    # print("KKKKKKKKKKKKKKKKK NOT FOUND")
+    # Console.quit("Image not found!")
     return None
