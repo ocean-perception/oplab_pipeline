1448b46ebeadb39e593ba7e9f7e58f09d23739b9
David Stanley <d.stanley@soton.ac.uk>
2022-03-10 11:03:45 +0000

    Update primary documentation of parser script

--- src/auv_nav/parsers/koyo20rov.py
+++ src/auv_nav/parsers/koyo20rov.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 """
-07-03-2022
-David Stanley
-koyo20rov.py
+Copyright (c) 2022, University of Southampton
+All rights reserved.
+Licensed under the BSD 3-Clause License.
+See LICENSE.md file in the project root for full license information.
 """
-# Borrowing heavily from the hybis.py parser in auv_nav
 
 
 from pathlib import Path
