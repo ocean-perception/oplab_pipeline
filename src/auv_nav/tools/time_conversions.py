ocean-perception/oplab_pipeline

commit d92432fc61312e40ebb369238b5921ee5c81162b

Allow date_time_to_epoch to use microseconds

src/auv_nav/tools/time_conversions.py
@@ -6,7 +6,6 @@ Licensed under the BSD 3-Clause License.
 See LICENSE.md file in the project root for full license information.
 """
 
-import calendar
 import time
 from datetime import datetime, timedelta, timezone
 
@@ -15,11 +14,20 @@ import pytz
 from oplab.console import Console
 
 
-def date_time_to_epoch(yyyy, mm, dd, hh, mm1, ss, timezone_offset_to_utc=0):
-    utc_date_time = datetime(yyyy, mm, dd, hh, mm1, ss) - timedelta(
+def date_time_to_epoch(
+    yyyy,
+    mm,
+    dd,
+    hh,
+    mm1,
+    ss,
+    timezone_offset_to_utc=0,
+    us=0,
+):
+    utc_date_time = datetime(yyyy, mm, dd, hh, mm1, ss, us) - timedelta(
         hours=timezone_offset_to_utc
     )
-    epochtime = calendar.timegm(utc_date_time.timetuple())
+    epochtime = utc_date_time.timestamp()
     return epochtime
 
 

commit 88e1e26f3dacb2da880393c758398cd7035ede76

Fix bug introduced in commit d92432fc where time is interpreted as local time

src/auv_nav/tools/time_conversions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2020, University of Southampton
+Copyright (c) 2022, University of Southampton
 All rights reserved.
 Licensed under the BSD 3-Clause License.
 See LICENSE.md file in the project root for full license information.
@@ -24,9 +24,9 @@ def date_time_to_epoch(
     timezone_offset_to_utc=0,
     us=0,
 ):
-    utc_date_time = datetime(yyyy, mm, dd, hh, mm1, ss, us) - timedelta(
-        hours=timezone_offset_to_utc
-    )
+    utc_date_time = datetime(
+        yyyy, mm, dd, hh, mm1, ss, us, tzinfo=timezone.utc
+    ) - timedelta(hours=timezone_offset_to_utc)
     epochtime = utc_date_time.timestamp()
     return epochtime
 