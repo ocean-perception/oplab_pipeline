895988fcf6664579998b54333de67be9f1e24224
Adrian Bodenmann <adrian.bodenmann@soton.ac.uk>
2023-03-27 17:29:54 +0000

    Update year

--- src/oplab/console.py
+++ src/oplab/console.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2020, University of Southampton
+Copyright (c) 2023, University of Southampton
 All rights reserved.
 Licensed under the BSD 3-Clause License.
 See LICENSE.md file in the project root for full license information.
@@ -152,7 +152,7 @@ class Console:
             + " University of Southampton"
         )
         print(" ")
-        print(" Copyright (C) 2020 University of Southampton   ")
+        print(" Copyright (C) 2023 University of Southampton   ")
         print(" This program comes with ABSOLUTELY NO WARRANTY.")
         print(" This is free software, and you are welcome to  ")
         print(" redistribute it.                               ")
