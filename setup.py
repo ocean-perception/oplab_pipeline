94be7d341d10016707de2796b1f092388c63106a
Adrian Bodenmann <adrian.bodenmann@soton.ac.uk>
2023-03-30 15:28:22 +0000

    Add content type in metadata, used by PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2020, University of Southampton
+Copyright (c) 2023, University of Southampton
 All rights reserved.
 Licensed under the BSD 3-Clause License.
 See LICENSE.md file in the project root for full license information.
@@ -118,6 +118,7 @@ def run_setup():
         author_email="miquel.massot-campos@soton.ac.uk",
         description="Toolchain for AUV dive processing, camera calibration and image correction",  # noqa
         long_description=long_description,
+        long_description_content_type='text/markdown',
         url="https://github.com/ocean-perception/oplab_pipeline",
         packages=find_packages(where="src"),
         package_dir={"": "src"},
