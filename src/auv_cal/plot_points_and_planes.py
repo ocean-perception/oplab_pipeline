b626262b4ac6b6b2de572a0ca4b110c5d15a63f6
ArjanAttwal <aattwal24@gmail.com>
2023-07-28 09:42:18 +0000

    updated plot_points_and_planes
    Removed plot_pointcloud_and_planes.
    REINCLUDE!!!!
    
    Updated some parts to accept line not plane

--- src/auv_cal/plot_points_and_planes.py
+++ src/auv_cal/plot_points_and_planes.py
@@ -105,92 +105,7 @@ def plane_to_isosceles_trapezoid_grid(plane, ydistmin, ydistmax, zmin1, zmax1):
     return (-d - c * zz - b * yy) / a, yy, zz
 
 
-def plot_pointcloud_and_planes(pointclouds, planes, plot_path=None):
-    """Plots list of pointclouds and planes with plotly
-
-    Parameters
-    ----------
-    pointclouds : list of ndarray
-        List of (n x 3) ndarrays with coordintes of points to be plotted
-    planes : list of ndarray
-        List of ndarrays (vectors of length 4) containing plane parameters
-    plot_path: String or None, optional
-        Filename for storing plot as html. If None, plot is displayed but not
-        saved. Default: None.
-
-    Returns
-    -------
-        None
-    """
-
-    fig = go.Figure()
-
-    for i, pc in enumerate(pointclouds):
-        sample_size = min(10000, pc.shape[0])
-        indices = np.random.choice(pc.shape[0], sample_size, replace=False)
-        pc_rs = pc[indices]
-        marker_size = 1
-        if pc_rs.shape[0] < 10:
-            marker_size = 5
-        fig.add_trace(
-            go.Scatter3d(
-                x=pc_rs.T[0],
-                y=pc_rs.T[1],
-                z=pc_rs.T[2],
-                mode="markers",
-                marker=dict(size=marker_size),
-                showlegend=True,
-                name="pointcloud" + str(i),
-            )
-        )
-    cmin = 0
-    cmax = len(planes) - 1
-    colorscale = "rainbow"
-    for i, plane in enumerate(planes):
-        # x, y, z = plane_to_rectuangular_grid(plane, -6, 6, 4, 14)
-        x, y, z = plane_to_isosceles_trapezoid_grid(plane, 2, 8, 4, 14)
-        surfacecolor = i * np.ones(shape=z.shape)
-        fig.add_trace(
-            go.Surface(
-                x=x,
-                y=y,
-                z=z,
-                name="plane" + str(i),
-                surfacecolor=surfacecolor,
-                cmin=cmin,
-                cmax=cmax,
-                colorscale=colorscale,
-                showscale=False,
-                showlegend=True,
-            )
-        )
-    now = datetime.now()
-    fig.update_layout(title="Pointcloud and plane(s) generated on " + str(now))
-    if plot_path is None:
-        fig.show()
-    else:
-        fig.write_html(plot_path, auto_open=True)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("ply_file", help="Path to a PLY file containing a point cloud")
-    parser.add_argument(
-        "-p",
-        "--plane",
-        dest="plane",
-        type=str,
-        help="Plane coefficients separated by spaces",
-    )
-
-    args = parser.parse_args()
-
-    cloud = read_ply(args.ply_file)
-    coeffs = args.plane.split(" ")
-    coeffs = np.array([float(x) for x in coeffs])
-    plot_pointcloud_and_planes([cloud], [coeffs])
-
-def plot_pointcloud_and_Lines(pointclouds, lines, plot_path=None):
+def plot_pointcloud_and_lines(pointclouds, lines, plot_path=None):
     """Plots list of pointclouds and lines with plotly
 
     Parameters
@@ -260,6 +175,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     cloud = read_ply(args.ply_file)
-    coeffs = args.plane.split(" ")
+    coeffs = args.line.split(" ")
     coeffs = np.array([float(x) for x in coeffs])
     plot_pointcloud_and_lines([cloud], [coeffs])
