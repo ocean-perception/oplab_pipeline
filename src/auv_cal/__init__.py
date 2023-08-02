f3b0d44d4a756ca733f376adbf3c0caadee36a04
ArjanAttwal <aattwal24@gmail.com>
2023-08-02 12:04:51 +0000

    Updated __init__.py
    readded import for class plane from plane fitting

--- src/auv_cal/__init__.py
+++ src/auv_cal/__init__.py
@@ -4,3 +4,4 @@ from .calibration import MonoCalibrator  # noqa
 from .calibration import StereoCalibrator  # noqa
 from .cone_fitting import CircularCone  # noqa
 from .plane_fitting import Line  # noqa
+from .plane_fitting import Plane
