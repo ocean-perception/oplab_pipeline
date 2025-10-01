f395f30918caa4b360b0a704f29d4f4a49bf0e32
Adrian Bodenmann <adrian.bodenmann@soton.ac.uk>
2025-10-01 09:13:15 +0100

    Don't apply 'resolve()' to path so that apparent folder structure does not change (preserve raw/configuration/processed in path when symlinking upwards)

--- src/oplab/folder_structure.py
+++ src/oplab/folder_structure.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2023, University of Southampton
+Copyright (c) 2023-2025, University of Southampton
 All rights reserved.
 Licensed under the BSD 3-Clause License.
 See LICENSE.md file in the project root for full license information.
@@ -45,7 +45,6 @@ def change_subfolder(path: Path, prior: str, new: str) -> Path:
 
 
 def get_folder(path: Union[str, Path], name: str) -> Optional[Path]:
-    path = Path(path).resolve(strict=False)
     if name in path.parts:
         return path
     elif "processed" in path.parts:
